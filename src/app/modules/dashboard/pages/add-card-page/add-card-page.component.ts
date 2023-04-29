741bae9f50f75210e8c4fd59142f03016af69edc
unknown <=>
2023-04-28 21:47:02 -0600

    Modify ER CURP

--- src/app/modules/dashboard/pages/add-card-page/add-card-page.component.ts
+++ src/app/modules/dashboard/pages/add-card-page/add-card-page.component.ts
@@ -85,7 +85,7 @@ export class AddCardPageComponent {
     this.alert.getStatus().failure.title = 'Ocurrió un error inesperado';
   }
   erCURP: string =
-    '[A-Z]{1}[AEIOU]{1}[A-Z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|1[0-9]|2[0-9]|3[0-1])[HM]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}[0-9]{1}';
+    '[A-Za-z]{1}[AEIOUaeiou]{1}[A-Za-z]{2}[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])[HMhm]{1}(AS|BC|BS|CC|CS|CH|CL|CM|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE|ne)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Za-z]{1}[0-9]{1}';
   ngOnInit() {
     this.formNewCard = new FormGroup({
       curp: new FormControl('', [
